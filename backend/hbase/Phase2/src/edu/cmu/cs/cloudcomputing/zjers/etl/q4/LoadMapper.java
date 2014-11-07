55d0874c5387dacda6abf47e37be9ce64706aa08
MyMiracle <kp26th@gmail.com>
2014-11-06 22:29:40 -0500

    new q4

--- backend/hbase/Phase2/src/edu/cmu/cs/cloudcomputing/zjers/etl/q4/LoadMapper.java
+++ backend/hbase/Phase2/src/edu/cmu/cs/cloudcomputing/zjers/etl/q4/LoadMapper.java
@@ -10,15 +10,18 @@ public class LoadMapper extends Mapper<LongWritable, Text, Text, Text> {
 	@Override
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		System.out.println("Map In----------");
+		if (value.getLength() == 0) return;
 		String line = value.toString();
 		Extract extract = Extract.create(line);
 			
 		if (extract != null) {
-			Text k = new Text(extract.timeStamp + ";" + extract.location);
+			Text k = new Text(extract.timeStamp + extract.location);
 			
 			for(String text: extract.tags){
-				Text v = new Text(text + ":" + extract.tid);
+				String offset = text.split("\\.")[1];
+				String tag = text.split("\\.")[0];
+				
+				Text v = new Text(tag + ":" + extract.tid + "." + offset);
 				
 				context.write(k, v);
 			}
