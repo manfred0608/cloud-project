b32061a3c9e3482c0196d58e8a3219efd0a462dc
yogenpro <pro.zju@gmail.com>
2014-11-07 22:18:44 -0500

    readme updated

--- backend/mysql/twitter/tweets-init.sql
+++ backend/mysql/twitter/tweets-init.sql
@@ -10,3 +10,29 @@ DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci
 DATA DIRECTORY='/data/'
 INDEX DIRECTORY='/data/';
+
+CREATE TABLE `retweets` (
+  `id` bigint(64) NOT NULL,
+    `response` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+  )
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+DATA DIRECTORY = '/data'
+INDEX DIRECTORY = '/data'
+;
+
+CREATE TABLE `hashtags` (
+  `timeLocation` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `rank` int(8) NOT NULL DEFAULT 1,
+  `hashtag` varchar(128) NOT NULL DEFAULT '',
+    `tweet_id` bigint(64) NOT NULL,
+  KEY `time_location` (`time_location`) USING HASH
+  )
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+DATA DIRECTORY = '/data'
+INDEX DIRECTORY = '/data'
+;
--- frontend/undertow/authserver/README.md
+++ frontend/undertow/authserver/README.md
@@ -10,3 +10,5 @@ RUN
 ===
 * Q1: ``java -cp target/uber-authserver-0.0.1-SNAPSHOT.jar authserver.QueryServer <port> <ip-addr-or-hostname-to-bind>``
 * Q2-MySQL: ``java -cp target/uber-authserver-0.0.1-SNAPSHOT.jar dbserver.QueryServer <port> <ip-addr-or-hostname-to-bind>``
+* Q3-MySQL: ``java -cp target/uber-authserver-0.0.1-SNAPSHOT.jar retweetserver.RetweetServer <port> <ip-addr-or-hostname-to-bind>``
+* Mixed-Q1Q2Q3Q4: ``java -cp target/uber-authserver-0.0.1-SNAPSHOT.jar mixedserver.FourMixedServer <port> <ip-addr-or-hostname-to-bind>``
